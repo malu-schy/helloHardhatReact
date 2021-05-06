fb9cae6e4be70e85a8a6d17160b353e865c5d079
Malu Schymanski <maluschymanski@Malus-MBP.fritz.box>
2021-05-06 15:08:32 +0200

    :wrench: compiled greeter, added path to artifacts directory in config, copied folder artifacts in cliend to access later from react app
    compiled greeter, added path to artifacts directory in config, copied folder artifacts in cliend to access later from react app

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -18,8 +18,11 @@ task("accounts", "Prints the list of accounts", async () => {
  */
  module.exports = {
   solidity: "0.8.3",
-  networks:{
-    hardhat:{
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
       chainId: 1337
     }
   }
