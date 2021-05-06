257caba99b9b79cbcca96aac290dc19b05d392bd
Malu Schymanski <maluschymanski@Malus-MBP.fritz.box>
2021-05-06 15:24:34 +0200

    :construction: wrote functions to read from bc and to write into bc
    wrote functions to read from bc and to write into bc

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Greeter from '../artifacts/contracts/Greeter.sol/Greeter.json';
+import { ethers } from 'ethers'
 
 function Home() {
+
+    const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    const [greeting, setNewGreeting] = useState('');
+    const [outputText, setOutputText ] = useState('');
+
+    async function requestAccount(){
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        console.log("called requestAccount");
+    }
+
+    async function getGreeting(){
+        if(typeof window.ethereum !== 'undefined'){
+            const provider = new ethers.providers.Web3Provider(window.ethereum)
+            console.log({provider});
+            const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+            try{
+                const data = await contract.greet()
+                console.log("data: ", data)
+                setOutputText(data);
+            } catch (err){
+                console.log("err: ",err)
+                setOutputText(err);
+            }
+        }
+    }
+
+
+    async function setGreeting(){
+        
+        if(!greeting) return
+        if(typeof window.ethereum !== 'undefined') { 
+            await requestAccount()
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            console.log({provider});
+            const signer = provider.getSigner()
+            const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
+            const transaction = await contract.setGreeting(greeting)
+            await transaction.wait()
+            getGreeting();
+        }
+    }
+
     return (
         <div>
-            <h1> Hello React </h1>
+            <div>
+                <div className="Headline">
+                <h1>Hello React &amp; Hardhat</h1>
+                </div>
+                <div className="Greeter">
+                <input onChange={e => setNewGreeting(e.target.value)} ></input>
+                <button onClick={setGreeting}>Save Greeting</button>
+                <button onClick={getGreeting}>Show Greeting</button>
+                </div>
+                <div>
+                    {outputText}
+                </div>
+            </div>
         </div>
     )
 }
